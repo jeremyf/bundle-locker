f0a84ceb0449d323e20ae68077c4d1ea443a9632
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-12-14 15:09:49 -0500

    Adding Gemfile parser

--- lib/bundle-locker.rb
+++ lib/bundle-locker.rb
@@ -1,7 +1,43 @@
 require "bundle-locker/version"
-
+require 'bundler'
 module Bundle
   module Locker
-    # Your code goes here...
+    class Parser
+      def initialize(gemfile_name)
+        @gemfile_name = gemfile_name
+        @gemfile_lock = Bundler::LockfileParser.new(File.read("#{@gemfile_name}.lock"))
+      end
+
+      def call
+        gem_dictionary = {}
+        File.readlines(@gemfile_name).each do |line|
+          if /\A(?<prefix>\s*gem\s+['"])(?<gem_name>[^'"]+)(?<suffix>['"])/ =~ line
+            gem_dictionary[gem_name] = {prefix: prefix, original: line, suffix: suffix, gem_name: gem_name}
+          end
+        end
+
+        @gemfile_lock.specs.each do |spec|
+          if gem_dictionary[spec.name]
+            gem_dictionary[spec.name][:locked_version] = spec.version.to_s
+            gem_dictionary[spec.name][:source] = spec.source.to_s
+          end
+        end
+        contents = File.read(@gemfile_name)
+        File.open(@gemfile_name, 'w+') do |file|
+          gem_dictionary.each do |key, dictionary|
+            if dictionary[:locked_version]
+              replacement = "#{dictionary[:prefix]}#{dictionary[:gem_name]}#{dictionary[:suffix]}, "
+              if dictionary[:source] =~ /git/
+                replacement << ":git => '#{dictionary[:source].sub(/\s.*\Z/,'')}'\n"
+              else
+                replacement << "'#{dictionary[:locked_version]}'\n"
+              end
+              contents.gsub!(dictionary[:original], replacement)
+            end
+          end
+          file.puts contents
+        end
+      end
+    end
   end
 end
